2ec472805d04bfa4963f0b877a8e87967ce34c57
Michael <2090103607@qq.com>
2025-12-31 23:06:34 +0800

    Cleanup: Remove all untracked files and folders

--- ai_learn/realtime_translator/extension/background.js
+++
@@ -1,5 +0,0 @@
-// Background script
-// Not strictly needed for this simple flow but good for persistence
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("AI Translator Extension Installed");
-});
--- debug_context.py
+++
@@ -1,8 +0,0 @@
-
-from llama_index.core.workflow import Context
-import inspect
-
-print("Context methods:")
-for name, method in inspect.getmembers(Context):
-    if not name.startswith("_"):
-        print(name)
